31a985215129a6c1e949c01a5d18dd14eccf78db
Leonardo1596 <leonardoemanuel156@gmail.com>
2024-12-19 05:14:19 -0300

    changed route

--- src/pages/Login/Index.js
+++ src/pages/Login/Index.js
@@ -16,7 +16,7 @@ const Index = () => {
                 email,
                 password
             };
-            const response = await axios.post('http://localhost:8000/auth/sign-up', body);
+            const response = await axios.post('https://insta-1d51.onrender.com/auth/sign-up', body);
         } catch (err) {
 
         }
